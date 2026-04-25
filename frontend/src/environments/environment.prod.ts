c62aa8618372fbe13848ab42d0a8e67c7425a1a4
Vishnu M <vishnumagesh12@gmail.com>
2026-04-25 16:51:39 +0530

    Separated local and production environments

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: 'http://13.236.185.202:8080'
+};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'http://13.236.185.202:8080'
+  apiUrl: 'http://localhost:8080'
 };
